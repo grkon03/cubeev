754fc5090fcf099a102956d46d072dda07cc6fbf
grkon <yo.for.user@gmail.com>
2021-11-12 10:39:26 +0900

    cmake 環境を整えた

---
+++ build/CMakeFiles/cubeev.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cubeev.dir/main.cpp.o"
+  "CMakeFiles/cubeev.dir/main.cpp.o.d"
+  "cubeev"
+  "cubeev.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cubeev.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/cubeev.dir/main.cpp.o.d
@@ -0,0 +1,1 @@
+CMakeFiles/cubeev.dir/main.cpp.o: /Users/ousuke/cubeev/main.cpp
