3fc3bbdd86505a43a669dc9513b57666a87634aa
grkon <yo.for.user@gmail.com>
2021-11-20 18:15:56 +0900

    main を整備した

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include "evaluator/cevaluator.hpp"
 #include "evaluator/csquares.hpp"
 
-int main() {
+/*
+第一引数では、ファイル名を
+*/
+int main(int argc, char *argv[]) {
+    // line.dat データの初期化
+    data_init<LINE_DAT_TYPE>(LINE_DAT_ID, false);
+
+    if (argc == 1) {
+
+    } else {
+
+    }
+
     return 0;
 }
